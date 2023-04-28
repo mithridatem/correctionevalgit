8afff272f8b05222227c407aa79b4ce03e69938b
mithridatem <mathieumithridate@adrar-formation.com>
2023-04-28 12:12:04 +0200

    update view_connexion.php prenom

---
+++ view/view_connexion.php
@@ -0,0 +1,20 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style.css">
+    <script src="./script/script.js"defer></script>
+    <title>Connexion</title>
+</head>
+<body>
+    <h1>Connexion : </h1>
+    <form action="" method="post">
+        <p>Veuillez saisir votre identifiant :</p>
+        <input type="text" name="login">
+        <p>Veuillez saisir votre mot de passe :</p>
+        <input type="text" name="mdp">
+        <input type="submit" value="connexion">
+    </form>
+</body>
+</html>
